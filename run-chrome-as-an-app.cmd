2aa6ce4bbeafdc0130353639e0f810292a1b5066
Dirk Arnez <smalldirkalex@gmail.com>
2022-05-19 23:00:53 +0800

    - init

---
+++ run-chrome-as-an-app.cmd
@@ -0,0 +1,2 @@
+"C:\Program Files\Google\Chrome\Application\chrome.exe" --chrome-frame --window-size=800,60 --window-position=580,240 --app=http://www.google.com/ || "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --chrome-frame --window-size=800,60 --window-position=580,240 --app=http://www.google.com/
+pause
