cd8c5133210d1023d8b0f6646eb11ad1fe7098db
Dirk Arnez <smalldirkalex@gmail.com>
2022-05-20 17:53:37 +0800

    - update run-chrome-as-an-app.cmd

--- run-chrome-as-an-app.cmd
+++ run-chrome-as-an-app.cmd
@@ -1,2 +1,7 @@
-"C:\Program Files\Google\Chrome\Application\chrome.exe" --chrome-frame --window-size=800,60 --window-position=580,240 --app=http://www.google.com/ || "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --chrome-frame --window-size=800,60 --window-position=580,240 --app=http://www.google.com/
-pause
+set CHROME="C:\Program Files\Google\Chrome\Application\chrome.exe"
+
+IF NOT EXIST %CHROME% (
+	set CHROME="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+)
+
+start "" %CHROME% --chrome-frame --window-size=800,600 --window-position=580,240 --app=http://www.google.com/
